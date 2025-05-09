f8bc093884f2a8c9f959c1e0612d55a239dba6ac
teddyseptiansyah <teddyseptiansyah@gmail.com>
2025-05-09 23:34:32 +0700

    Delete docker.sh

--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker build -t telegram-deepseek-bot .
-docker tag telegram-deepseek-bot jackyin0822/telegram-deepseek-bot:${1}
-docker push  jackyin0822/telegram-deepseek-bot:${1}
-docker tag telegram-deepseek-bot jackyin0822/telegram-deepseek-bot:latest
-docker push  jackyin0822/telegram-deepseek-bot:latest
-
